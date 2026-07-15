1e9823f1a88c2586effec819c594ff7df56edff7
Maksim Zhigarev <32813820+MrSmile13@users.noreply.github.com>
2026-07-15 13:02:31 +0300

    Обновление версии фреймворка до 1.5.4 (#4)

--- src/main/resources/archetype-resources/src/main/java/ru/example/steps/fixtures/BeforeAllTestsExtension.java
+++ src/main/resources/archetype-resources/src/main/java/ru/example/steps/fixtures/BeforeAllTestsExtension.java
@@ -5,18 +5,12 @@ package ru.example.steps.fixtures;
 
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import ru.example.steps.kafka.KafkaConsumerSteps;
-import ru.example.steps.kafka.KafkaProducerSteps;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BeforeAllTestsExtension implements BeforeAllCallback {
 
     private static final AtomicBoolean FIRST_EXECUTE = new AtomicBoolean(true);
-
-    private static KafkaProducerSteps producerSteps = new KafkaProducerSteps();
-
-    private static KafkaConsumerSteps consumerSteps = new KafkaConsumerSteps();
 
     @Override
     public void beforeAll(ExtensionContext context) {
